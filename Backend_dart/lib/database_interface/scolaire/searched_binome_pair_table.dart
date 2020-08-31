fe83710da2a7395cd7b938bf0add55d9e4e8475f
Lulupointu <lucasdelsol@hotmail.fr>
2020-08-31 19:00:12 +0200

    Fixing SQL request

--- Backend_dart/lib/database_interface/scolaire/searched_binome_pair_table.dart
+++ Backend_dart/lib/database_interface/scolaire/searched_binome_pair_table.dart
@@ -16,12 +16,18 @@ class SearchedBinomePairsTable {
 
   Future<Map<String, SearchedBinomePair>> getAllExceptOneFromLogin({@required String login}) async {
     final Future query = database.mappedResultsQuery(
-        "SELECT ${BinomePairsProfilesTable.name}.\"binomePairId\", name, surname, \"otherName\", \"otherSurname\", \"status\" FROM "
-            "${RelationsStatusBinomePairsMatchesTable.name} "
-            "JOIN ("
-            " SELECT * FROM ${BinomePairsProfilesTable.name} "
-            "WHERE login=@login OR \"otherLogin\"=@login"
-            "ON  ${RelationsStatusBinomePairsMatchesTable.name}",
+        "SELECT \"binomePairId\", name, surname, \"otherName\", \"otherSurname\", \"status\" "
+            " FROM "
+            "( SELECT \"otherBinomePairId\" AS \"binomePairId\", status FROM "
+            " ${RelationsStatusBinomePairsMatchesTable.name} "
+            "JOIN "
+            "( SELECT \"binomePairId\" FROM ${BinomePairsProfilesTable.name} "
+            "WHERE login=@login OR \"otherLogin\"=@login "
+            ") AS ${BinomePairsProfilesTable.name} "
+            "USING (\"binomePairId\") "
+            ") AS ${RelationsStatusBinomePairsMatchesTable.name} "
+            "JOIN ${BinomePairsProfilesTable.name} "
+            "USING (\"binomePairId\") ",
         substitutionValues: {
           'login': login,
         });
@@ -29,7 +35,7 @@ class SearchedBinomePairsTable {
     return query.then((queryResults) {
       return {
         for (Map<String, Map<String, dynamic>> queryResult in queryResults)
-          queryResult[UsersTable.name]['binomePairId']: SearchedBinomePair.fromJson({
+          queryResult[BinomePairsProfilesTable.name]['binomePairId']: SearchedBinomePair.fromJson({
             ...queryResult[UsersTable.name],
             'liked': _getLikeOrNotFromRelationStatusBinomePair(
                 EnumRelationStatusBinomePair.valueOf(queryResult[RelationsStatusBinomePairsMatchesTable.name]['statusBinomePair']))
