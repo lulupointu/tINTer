62182b9206ebfd6a880dfbccfd120d7d57f9843c
Lulupointu <lucasdelsol@hotmail.fr>
2020-09-02 12:20:53 +0200

    Fixing type in file name

--- Backend_dart/lib/main.dart
+++ Backend_dart/lib/main.dart
@@ -7,7 +7,7 @@ Logger _serverLogger = Logger('Server');
 Future<void> main() async {
   Stream<HttpRequest> server;
 
-  File _logFile = File('~/logs');
+  File _logFile = File('/home/df/logs');
   IOSink _logFileSink = _logFile.openWrite();
 
   Logger.root.level = Level.ALL; // defaults to Level.INFO
