da015ca606e37cb4b637492d1885d68c2ea21cc6
Lulupointu <lucasdelsol@hotmail.fr>
2020-09-09 12:10:40 +0200

    Completing missing scores and status

--- Backend_dart/lib/database_interface/add_missing_relations_scolaires.dart
+++ Backend_dart/lib/database_interface/add_missing_relations_scolaires.dart
@@ -1,56 +1,36 @@
-import 'package:meta/meta.dart';
-import 'package:postgres/postgres.dart';
 import 'package:tinter_backend/database_interface/associatif/relation_status_associatif_table.dart';
 import 'package:tinter_backend/database_interface/database_interface.dart';
-import 'package:tinter_backend/database_interface/scolaire/binome_pairs_management_table.dart';
-import 'package:tinter_backend/database_interface/scolaire/binome_pairs_score_table.dart';
-import 'package:tinter_backend/database_interface/scolaire/binome_pairs_status_table.dart';
 import 'package:tinter_backend/database_interface/scolaire/matieres_table.dart';
 import 'package:tinter_backend/database_interface/scolaire/relation_score_scolaire_table.dart';
 import 'package:tinter_backend/database_interface/scolaire/relation_status_scolaire_table.dart';
 import 'package:tinter_backend/database_interface/shared/associations_table.dart';
 import 'package:tinter_backend/database_interface/associatif/gouts_musicaux_table.dart';
-import 'package:tinter_backend/database_interface/associatif/matches_table.dart';
 import 'package:tinter_backend/database_interface/associatif/relation_score_associatif_table.dart';
-import 'package:tinter_backend/database_interface/shared/sessions.dart';
 import 'package:tinter_backend/database_interface/user_management_table.dart';
-import 'package:tinter_backend/models/associatif/association.dart';
-import 'package:tinter_backend/models/scolaire/relation_score_binome_pair.dart';
+import 'package:tinter_backend/models/associatif/relation_score_associatif.dart';
+import 'package:tinter_backend/models/associatif/relation_status_associatif.dart';
 import 'package:tinter_backend/models/scolaire/relation_score_scolaire.dart';
-import 'package:tinter_backend/models/scolaire/relation_status_binome_pair.dart';
 import 'package:tinter_backend/models/scolaire/relation_status_scolaire.dart';
 import 'package:tinter_backend/models/shared/user.dart';
-import 'package:tinter_backend/secret.dart';
 
-Future<void> main() async {
+main() async {
+  Future.wait([
+    addMissingScolaire(),
+    addMissingAssociatif(),
+  ]);
+}
+
+Future<void> addMissingScolaire() async {
   final TinterDatabase tinterDatabase = TinterDatabase();
   await tinterDatabase.open();
 
   final UsersManagementTable usersManagementTable =
   UsersManagementTable(database: tinterDatabase.connection);
-  final AssociationsTable associationsTable =
-  AssociationsTable(database: tinterDatabase.connection);
-  final GoutsMusicauxTable goutsMusicauxTable =
-  GoutsMusicauxTable(database: tinterDatabase.connection);
-  final MatieresTable matieresTable = MatieresTable(database: tinterDatabase.connection);
-  final RelationsScoreAssociatifTable relationsScoreAssociatifTable =
-  RelationsScoreAssociatifTable(database: tinterDatabase.connection);
-  final RelationsStatusAssociatifTable relationsStatusAssociatifTable =
-  RelationsStatusAssociatifTable(database: tinterDatabase.connection);
-  final RelationsScoreScolaireTable relationsScoreScolaireTable =
-  RelationsScoreScolaireTable(database: tinterDatabase.connection);
   final RelationsStatusScolaireTable relationsStatusScolaireTable =
   RelationsStatusScolaireTable(database: tinterDatabase.connection);
-  final MatchesTable matchesTable = MatchesTable(database: tinterDatabase.connection);
-  final SessionsTable sessionsTable = SessionsTable(database: tinterDatabase.connection);
-  final BinomePairsManagementTable binomePairsManagementTable = BinomePairsManagementTable(
-      database: tinterDatabase.connection);
-  final RelationsScoreBinomePairsMatchesTable relationsScoreBinomePairsMatchesTable =
-  RelationsScoreBinomePairsMatchesTable(database: tinterDatabase.connection);
-  final RelationsStatusBinomePairsMatchesTable relationsStatusBinomePairsMatchesTable =
-  RelationsStatusBinomePairsMatchesTable(database: tinterDatabase.connection);
 
   Map<String, BuildUser> allUsers = await usersManagementTable.getAll(year: TSPYear.TSP1A, school: School.TSP);
+  print('Adding missing users scolaire');
   for (BuildUser user in allUsers.values) {
     try {
       Map<String,
@@ -92,6 +72,113 @@ Future<void> main() async {
             ..statusScolaire = EnumRelationStatusScolaire.none)
         ]
       ]);
+    } catch (e) {
+      print('Error with user ${user.login}: $e');
+    }
+  }
+
+  tinterDatabase.close();
+}
+
+Future<void> addMissingAssociatif() async {
+  final TinterDatabase tinterDatabase = TinterDatabase();
+  await tinterDatabase.open();
+
+  final UsersManagementTable usersManagementTable =
+  UsersManagementTable(database: tinterDatabase.connection);
+  final RelationsScoreAssociatifTable relationsScoreAssociatifTable =
+  RelationsScoreAssociatifTable(database: tinterDatabase.connection);
+  final RelationsStatusAssociatifTable relationsStatusAssociatifTable =
+  RelationsStatusAssociatifTable(database: tinterDatabase.connection);
+
+  Map<String, BuildUser> allUsersPrimoEntrant = await usersManagementTable.getAll(primoEntrant: true);
+  print('Adding missing users associatifs primoEntrant');
+  for (BuildUser user in allUsersPrimoEntrant.values) {
+    try {
+      Map<String,
+          RelationStatusAssociatif> relationsAssociatif = await relationsStatusAssociatifTable
+          .getAllFromLogin(login: user.login);
+      List<BuildUser> otherUsersAssociatifMissing = allUsersPrimoEntrant.values.where((BuildUser otherUser) =>
+      otherUser != user && !relationsAssociatif.keys.contains(otherUser.login)).toList();
+
+      print('${user.login} is missing ${otherUsersAssociatifMissing.map((BuildUser user) => user.login).toList()}');
+
+      // Get the number of associations and gouts musicaux
+      int numberMaxOfAssociations =
+          (await AssociationsTable(database: tinterDatabase.connection).getAll()).length;
+      int numberMaxOfGoutsMusicaux =
+          (await GoutsMusicauxTable(database: tinterDatabase.connection).getAll()).length;
+
+      // Get the scores
+      Map<String, RelationScoreAssociatif> scores =
+      RelationScoreAssociatif.getScoreBetweenMultiple(user, otherUsersAssociatifMissing,
+          numberMaxOfAssociations, numberMaxOfGoutsMusicaux);
+
+      // Update the database with all relevant scores
+      await relationsScoreAssociatifTable.addMultiple(
+          listRelationScoreAssociatif: scores.values.toList());
+
+      // Update the database with status none
+      await relationsStatusAssociatifTable.addMultiple(listRelationStatusAssociatif: [
+        for (String otherLogin in otherUsersAssociatifMissing.map((BuildUser user) => user.login).toList()) ...[
+          RelationStatusAssociatif((b) => b
+            ..login = user.login
+            ..otherLogin = otherLogin
+            ..status = EnumRelationStatusAssociatif.none),
+          RelationStatusAssociatif((b) => b
+            ..login = otherLogin
+            ..otherLogin = user.login
+            ..status = EnumRelationStatusAssociatif.none)
+        ]
+      ]);
+
+    } catch (e) {
+      print('Error with user ${user.login}: $e');
+    }
+  }
+
+
+  Map<String, BuildUser> allUsersNotPrimoEntrant = await usersManagementTable.getAll(primoEntrant: false);
+  print('Adding missing users associatifs not primoEntrant');
+  for (BuildUser user in allUsersNotPrimoEntrant.values) {
+    try {
+      Map<String,
+          RelationStatusAssociatif> relationsAssociatif = await relationsStatusAssociatifTable
+          .getAllFromLogin(login: user.login);
+      List<BuildUser> otherUsersAssociatifMissing = allUsersNotPrimoEntrant.values.where((BuildUser otherUser) =>
+      otherUser != user && !relationsAssociatif.keys.contains(otherUser.login)).toList();
+
+      print('${user.login} is missing ${otherUsersAssociatifMissing.map((BuildUser user) => user.login).toList()}');
+
+      // Get the number of associations and gouts musicaux
+      int numberMaxOfAssociations =
+          (await AssociationsTable(database: tinterDatabase.connection).getAll()).length;
+      int numberMaxOfGoutsMusicaux =
+          (await GoutsMusicauxTable(database: tinterDatabase.connection).getAll()).length;
+
+      // Get the scores
+      Map<String, RelationScoreAssociatif> scores =
+      RelationScoreAssociatif.getScoreBetweenMultiple(user, otherUsersAssociatifMissing,
+          numberMaxOfAssociations, numberMaxOfGoutsMusicaux);
+
+      // Update the database with all relevant scores
+      await relationsScoreAssociatifTable.addMultiple(
+          listRelationScoreAssociatif: scores.values.toList());
+
+      // Update the database with status none
+      await relationsStatusAssociatifTable.addMultiple(listRelationStatusAssociatif: [
+        for (String otherLogin in otherUsersAssociatifMissing.map((BuildUser user) => user.login).toList()) ...[
+          RelationStatusAssociatif((b) => b
+            ..login = user.login
+            ..otherLogin = otherLogin
+            ..status = EnumRelationStatusAssociatif.none),
+          RelationStatusAssociatif((b) => b
+            ..login = otherLogin
+            ..otherLogin = user.login
+            ..status = EnumRelationStatusAssociatif.none)
+        ]
+      ]);
+
     } catch (e) {
       print('Error with user ${user.login}: $e');
     }
