bd4d37bbd40b6e5b6e5bab1fa8470c4e6d2b78c1
Lulupointu <lucasdelsol@hotmail.fr>
2020-09-13 01:12:55 +0200

    testing hiding profile with binome for all

--- Backend_dart/lib/database_interface/scolaire/searched_user_scolaire_table.dart
+++ Backend_dart/lib/database_interface/scolaire/searched_user_scolaire_table.dart
@@ -1,5 +1,6 @@
 import 'package:logging/logging.dart';
 import 'package:postgres/postgres.dart';
+import 'package:tinter_backend/database_interface/scolaire/binome_pairs_profiles_table.dart';
 import 'package:tinter_backend/database_interface/scolaire/relation_status_scolaire_table.dart';
 import 'package:meta/meta.dart';
 import 'package:tinter_backend/database_interface/user_table.dart';
@@ -16,19 +17,41 @@ class SearchedUserScolaireTable {
     @required this.database,
   });
 
-  Future<Map<String, SearchedUserScolaire>> getAllExceptOneFromLogin({@required String login}) async {
+  Future<Map<String, SearchedUserScolaire>> getAllExceptOneFromLogin(
+      {@required String login}) async {
     _logger.info('Executing function getAllExceptOneFromLogin with args: login=${login}');
 
-    final Future query = database.mappedResultsQuery(
+    String getAllExceptOneFromLoginQuery =
         "SELECT ${UsersTable.name}.login, name, surname, \"statusScolaire\" FROM "
         "(SELECT * FROM ${RelationsStatusScolaireTable.name} "
         "WHERE login=@login "
         ") AS ${RelationsStatusScolaireTable.name} "
         "JOIN ${UsersTable.name} "
-        "ON ${RelationsStatusScolaireTable.name}.\"otherLogin\"=${UsersTable.name}.login ",
-        substitutionValues: {
-          'login': login,
-        });
+        "ON ${RelationsStatusScolaireTable.name}.\"otherLogin\"=${UsersTable.name}.login ";
+
+    if (login == "delsol_l") {
+      getAllExceptOneFromLoginQuery =
+          "SELECT ${UsersTable.name}.login, name, surname, \"statusScolaire\" FROM "
+
+          "(SELECT ${RelationsStatusScolaireTable.name}.login, ${RelationsStatusScolaireTable.name}.\"otherLogin\", ${RelationsStatusScolaireTable.name}.\"statusScolaire\" FROM "
+
+          "(SELECT * FROM ${RelationsStatusScolaireTable.name} "
+          "WHERE login=@login "
+          ") AS ${RelationsStatusScolaireTable.name} "
+
+          " LEFT OUTER JOIN ${BinomePairsProfilesTable.name} "
+          " ON ${BinomePairsProfilesTable.name}.login=${RelationsStatusScolaireTable.name}.\"otherLogin\" OR ${BinomePairsProfilesTable.name}.\"otherLogin\"=${RelationsStatusScolaireTable.name}.\"otherLogin\" "
+          " WHERE ${BinomePairsProfilesTable.name}.\"binomePairId\" IS NULL"
+          ") AS ${RelationsStatusScolaireTable.name} "
+
+          "JOIN ${UsersTable.name} "
+          "ON ${RelationsStatusScolaireTable.name}.\"otherLogin\"=${UsersTable.name}.login ";
+    }
+
+    final Future query =
+        database.mappedResultsQuery(getAllExceptOneFromLoginQuery, substitutionValues: {
+      'login': login,
+    });
 
     return query.then((queryResults) {
       return {
@@ -36,16 +59,19 @@ class SearchedUserScolaireTable {
           queryResult[UsersTable.name]['login']: SearchedUserScolaire.fromJson({
             ...queryResult[UsersTable.name],
             'liked': _getLikeOrNotFromRelationStatusScolaire(
-                EnumRelationStatusScolaire.valueOf(queryResult[RelationsStatusScolaireTable.name]['statusScolaire']))
+                EnumRelationStatusScolaire.valueOf(
+                    queryResult[RelationsStatusScolaireTable.name]['statusScolaire']))
           })
       };
     });
   }
 
   bool _getLikeOrNotFromRelationStatusScolaire(EnumRelationStatusScolaire relationStatus) {
-    _logger.info('Executing function _getLikeOrNotFromRelationStatusScolaire with args: relationStatus=${relationStatus}');
+    _logger.info(
+        'Executing function _getLikeOrNotFromRelationStatusScolaire with args: relationStatus=${relationStatus}');
 
-    return (relationStatus == EnumRelationStatusScolaire.ignored || relationStatus == EnumRelationStatusScolaire.none)
+    return (relationStatus == EnumRelationStatusScolaire.ignored ||
+            relationStatus == EnumRelationStatusScolaire.none)
         ? false
         : true;
   }
