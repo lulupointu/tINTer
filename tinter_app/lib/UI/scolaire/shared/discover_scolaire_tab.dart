06296d38bcfd048083f67eef3b2932cacea4e040
guillotmatthieu <matthieu.guillot@telecom-sudparis.eu>
2021-05-19 13:05:48 +0200

    fix QuadrinomeOverlay

--- tinter_app/lib/UI/scolaire/shared/discover_scolaire_tab.dart
+++ tinter_app/lib/UI/scolaire/shared/discover_scolaire_tab.dart
@@ -4,6 +4,7 @@ import 'package:tinterapp/Logic/blocs/scolaire/matched_binomes/binomes_bloc.dart
 import 'package:tinterapp/UI/scolaire/discover_binome_pair/discover_binome_pair.dart';
 import 'package:tinterapp/UI/shared/shared_element/const.dart';
 import 'package:tinterapp/UI2/scolaire/discover_binome/discover_binome2.dart';
+import 'package:tinterapp/UI2/scolaire/discover_quadrinome/discover_quadrinome2.dart';
 
 class DiscoverScolaireTab extends StatelessWidget implements TinterTab {
   @override
@@ -14,7 +15,7 @@ class DiscoverScolaireTab extends StatelessWidget implements TinterTab {
           return CircularProgressIndicator();
         return (matchedBinomesState as MatchedBinomesHasBinomePairCheckedSuccessState)
                 .hasBinomePair
-            ? DiscoverBinomePairTab()
+            ? DiscoverBinomePairTab2()
             : DiscoverBinomeTab2();
       },
     );
